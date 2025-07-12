545048ad402417436e94fb6fa24f39c877729548
GIT INDIA <gitechno.in@gmail.com>
2025-07-12 11:38:17 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,58 @@
+
+console.log('hhhh');
+// debugger;
+document.write('hellow loops kese ho!!!!');  // on display 
+// window.alert('ddfd');
+
+
+
+// LOOPS 
+// 1
+// 2
+// 3
+// 4
+// 5
+
+// for ( let i=0;i<=5;i++){
+//     document.write(i,'<Br>');
+// }
+
+
+// let a = 0;
+// while (a <=5){
+//     console.log(2*a);
+//     a++;
+// }
+
+// let b = 0;
+// do {
+//     ++b;
+//     console.log(2*b);
+    
+// }while (b<0);
+
+
+// 1- 100 
+// even or odd 
+
+
+// for ( let i=0;i<=100;i++){
+//     if (i %2==0){
+//     document.write("this is even ",i,'<Br>');
+//     }
+// }
+
+// for ( let i=0;i<=100;i++){
+//     if (i %2!=0){
+//     document.write("this is odd ",i,'<Br>');
+//     }
+// }
+
+for ( let i=0;i<=100;i++){
+    if (i %2==0){
+    document.write("<br>this is even ",i,'<Br>');
+    }
+    else{
+        console.log(i);
+    }
+}
